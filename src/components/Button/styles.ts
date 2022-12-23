168f0ab8c0e821013f07ba64bfbc0505288f9acb
Lucas Corrêa <65604927+correa0105@users.noreply.github.com>
2022-12-22 23:52:46 -0300

    Subindo desafio

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
-
 export const ButtonContainer = styled.button`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: #B8860B;
     color: #FFF;
 
-    border: 1px solid #81259D;
+    border: 1px solid #B8860B;
     border-radius: 21px;
 
     &:hover {
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -28,7 +28,7 @@ export const LoginContainer = styled.div`
 export const Title = styled.h1`
     font-size: 42px;
     font-weight: 700;
-    color: #81259D;
+    color: #B8860B;
 `
 
 export const Row = styled.div`
@@ -48,4 +48,3 @@ export const Column = styled.div`
 export const Spacing = styled.div`
     margin: 10px 0;
 `
-
